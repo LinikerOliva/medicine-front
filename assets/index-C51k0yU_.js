ec3f0c28f15dc2fa2ae8f68f841e489c68f65aca
LinikerOliva <LinikerOliva@users.noreply.github.com>
2025-11-27 23:50:41 +0000

    deploy: 8349f10bd6ead4eb82d180c6a46fe818070a96fd

--- assets/index-C51k0yU_.js
+++ assets/index-C51k0yU_.js
@@ -1,4 +1,4 @@
-import{g as is}from"./index-DD3GNlR9.js";/*! *****************************************************************************
+import{g as is}from"./index-CIVwFY-C.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
