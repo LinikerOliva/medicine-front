e9c773201385e5c0c6b7f26065c151385d456286
Liniker <liniker.oliva123@gmail.com>
2025-12-01 21:38:53 -0300

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, Navigate } from "react-router-dom";
 
 // Contexts
 import { AuthProvider } from "./contexts/auth-context";
