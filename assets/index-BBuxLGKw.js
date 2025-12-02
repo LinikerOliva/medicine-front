1c1d99baff3dd61e66f1dcacf8b9dd2b13b2312c
LinikerOliva <LinikerOliva@users.noreply.github.com>
2025-12-02 10:35:17 +0000

    deploy: 8793bea40fcf7c6ca94ba130504eec8ba1c9c24d

--- assets/index-BBuxLGKw.js
+++ assets/index-BBuxLGKw.js
@@ -1,4 +1,4 @@
-import{g as is}from"./index-BKKIGPd4.js";/*! *****************************************************************************
+import{g as is}from"./index-BFTWKSnu.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
