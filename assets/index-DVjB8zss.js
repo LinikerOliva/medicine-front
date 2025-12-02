9f63fd244dd8f994ce9bdd0572dc73fc679986c3
LinikerOliva <LinikerOliva@users.noreply.github.com>
2025-12-02 01:14:16 +0000

    deploy: 8793bcdd92cafc2f2ebbc476f0c72a948903657c

--- assets/index-DVjB8zss.js
+++ assets/index-DVjB8zss.js
@@ -1,4 +1,4 @@
-import{g as is}from"./index-BYCvtnno.js";/*! *****************************************************************************
+import{g as is}from"./index-Bg4jN7l2.js";/*! *****************************************************************************
 Copyright (c) Microsoft Corporation.
 
 Permission to use, copy, modify, and/or distribute this software for any
